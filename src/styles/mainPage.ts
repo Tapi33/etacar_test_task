a5284ae2f4d6c9a99012046b7b59d9835a82ae18
Tapi33 <artem8554134@gmail.com>
2022-12-09 12:14:55 +0300

    feat: add components and styles

---
+++ src/styles/mainPage.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledMainPage = styled.main`
+  width: 100%;
+  height: calc(100vh - 120px);
+  background-color: rgba(254, 246, 160, 0.98);
+`
+export const MainPageContent = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+`
