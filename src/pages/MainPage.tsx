a7330fb5991c0ff0fc57d64548c7c9a557fe9333
Tapi33 <artem8554134@gmail.com>
2022-12-09 22:12:18 +0300

    feat: create mainPage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import {StyledMainPage, MainPageContent} from "../styles/mainPage";
+import {StyledMainPage, MainPageContent} from '../styles/mainPage';
 import {Wrapper} from "../styles/header";
+import {CurrencyList} from "../components/main/CurrencyList";
 
 export const MainPage = () => {
     return (
         <StyledMainPage>
             <Wrapper>
                 <MainPageContent>
-
+                    <CurrencyList />
                 </MainPageContent>
             </Wrapper>
         </StyledMainPage>
--- src/styles/mainPage.ts
+++ src/styles/mainPage.ts
@@ -9,5 +9,5 @@ export const MainPageContent = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
-  justify-content: center;
+  align-items: center;
 `
